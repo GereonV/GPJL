3f4772cd224e8991b5f2282075e3ffb696665d08
Gereon V <GereonV@users.noreply.github.com>
2021-06-21 23:01:17 +0200

    documented network.production examples

--- gevi/network/production/MyClient.java
+++ gevi/network/production/MyClient.java
@@ -5,28 +5,31 @@ import gevi.network.api.Client;
 public class MyClient extends Client {
 
     public static void main(String[] args) throws InterruptedException {
-        Client client = new MyClient("localhost");
-        client.sendString("Test from Client");
-        client.sendEnd();
+        Client client = new MyClient("localhost");  //create a Client-Object - replace localhost with IP
+        if(!client.connect(3636))   //try to connect to a ServerSocket
+            return;
+        client.sendString("Test from Client");  //send Data
+        Thread.sleep(1000);
+        client.sendEnd();   //close the Connection
     }
 
     public MyClient(String hostname) {
-        super(hostname);
+        super(hostname);    //push through to Client-Class
     }
 
     @Override
     protected void readString(String string) {
-        System.out.println(string);
+        System.out.println(string); //print incoming Strings
     }
 
     @Override
     protected void readObject(Object object) {
-        System.out.println("Received an Object");
+        System.out.println("Received an Object");   //log that an Object has been sent
     }
 
     @Override
     protected void onDisconnect() {
-        System.out.println("Server closed Connection");
+        System.out.println("Server closed Connection"); //log that the Server closed the Connection
     }
     
 }
--- gevi/network/production/MyServer.java
+++ gevi/network/production/MyServer.java
@@ -5,25 +5,25 @@ import gevi.network.api.Server;
 public class MyServer extends Server {
 
     public static void main(String[] args) throws InterruptedException {
-        Server server = new MyServer();
-        server.connect(3636);
-        Thread.sleep(2000);
-        server.sendString("Test from Server");
+        Server server = new MyServer(); //create a Server-Object
+        server.connect(3636);   //open ServerSocket for a new Connection
+        server.sendString("Test from Server");  //send Data after connection has been established
     }
 
     @Override
     protected void readString(String string) {
-        System.out.println(string);
+        System.out.println(string); //print incoming Strings
     }
 
     @Override
     protected void readObject(Object object) {
-        System.out.println("Received an Object");
+        System.out.println("Received an Object");   //log that an Object has been sent
     }
 
     @Override
     protected void onDisconnect() {
-        System.out.println("Client closed Connection");
+        System.out.println("Client closed Connection\n");   //log that the Client closed the Connection
+        connect(3636);  //reopen for a new Connection
     }
 
 }
