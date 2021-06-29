f057bab85398a74194c5dd3d6d458145ecb60d55
GereonV <GereonV@users.noreply.github.com>
2021-06-29 14:43:08 +0200

    Reworked IntegerGeneration Example

--- example/random/IntGenerator.java
+++ example/random/IntGenerator.java
@@ -4,14 +4,15 @@ import gpjl.random.IntegerGenerator;
 import gpjl.random.IntegerRange;
 import gpjl.random.InvalidRangeException;
 
-public class Generator {
+public class IntGenerator {
 
     public static void main(String[] args) throws InvalidRangeException {
-        IntegerGenerator random = new IntegerGenerator(new IntegerRange(1, 3));
+        IntegerGenerator random = new IntegerGenerator(new IntegerRange(1, 3)); //creates a new IntegerGenerator-Object
         for(int i = 0; i < 6; i++)
             System.out.println(random.nextInt(true));
         for(int i = 0; i < 5; i++)
             System.out.println(random.nextInt(false));
+        random.getRange().setMax(6);    //change one Range-Attribute
         for(int i = 0; i < 6; i++)
             System.out.println(random.nextInt(true));
     }
